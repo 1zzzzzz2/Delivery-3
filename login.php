a0061e7b1980333dcb96b961cdb38058f4ab8773
1zzzzzz2 <2048647950@qq.com>
2026-05-09 17:28:55 +0800

    Write a processing query user

--- login.php
+++ login.php
@@ -17,6 +17,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
 
+    $sql = "SELECT * FROM sellers WHERE username = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $user = mysqli_fetch_assoc($result);
+
 }
 ?>
 <!DOCTYPE html>
